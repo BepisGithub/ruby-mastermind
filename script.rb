a3738db30af3558a725359212356d97a0221747b
bepis <luqdek@gmail.com>
2021-01-17 12:33:29 +0000

    Added comments

--- script.rb
+++ script.rb
@@ -1,3 +1,9 @@
+# I don't have a player class to inherit from at the time of writing this comment
+# THis is because I don't see any shared functionality between them
+# The code maker must generate and store a code, then compare a given input with it and output the response
+# The code breaker must create a sequence and the game must pass it to see if it matches up
+# If it does then the breaker wins but if not then the maker returns some information about the positions of the colours
+
 class CodeBreaker
 
 end
